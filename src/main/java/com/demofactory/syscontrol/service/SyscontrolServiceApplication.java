f18e5070552adc77b62afe77f34c5c63fc826df3
Mad98 <w_yyy@outlook.com>
2020-08-13 01:37:41 +0800

    init repo

---
+++ src/main/java/com/demofactory/syscontrol/service/SyscontrolServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demofactory.syscontrol.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyscontrolServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyscontrolServiceApplication.class, args);
+    }
+
+}
