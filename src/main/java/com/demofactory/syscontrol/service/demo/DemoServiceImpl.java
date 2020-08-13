6e6efe2d3dd8e15951c5ca316ccc942a0948d2a4
Mad98 <w_yyy@outlook.com>
2020-08-13 15:34:41 +0800

    初始化工程、暂未集成mybatis

--- src/main/java/com/demofactory/syscontrol/service/demo/DemoServiceImpl.java
+++ src/main/java/com/demofactory/syscontrol/service/demo/DemoServiceImpl.java
@@ -1,7 +1,15 @@
 package com.demofactory.syscontrol.service.demo;
 
+import com.demofactory.syscontrol.api.DemoService;
 import com.demofactory.syscontrol.domain.demo.Demo;
+import org.springframework.stereotype.Service;
 
-public class DemoServiceImpl {
-    private Demo demo;
+@Service
+public class DemoServiceImpl implements DemoService {
+
+
+    @Override
+    public int insert(Demo demo) {
+        return 0;
+    }
 }
