4a03b35fb5192baa318da02bf307dcfa19e1afe4
fuhanxiang <FHX100279a>
2020-08-14 16:10:12 +0800

    测试注册功能

--- src/main/java/com/demofactory/syscontrol/service/demo/SysUserServiceImpl.java
+++ src/main/java/com/demofactory/syscontrol/service/demo/SysUserServiceImpl.java
@@ -13,9 +13,33 @@ public class SysUserServiceImpl implements SysUserService {
     @Resource
     private SysUserDao sysUserDao;
 
-    @Override
-    public SysUser loginByAccount(String account) {
 
-        return sysUserDao.findByAccount(account);
+    @Override
+    public SysUser loginByAccountAndPassword(String account, String password) {
+
+        SysUser sysUser = sysUserDao.findByAccount(account);
+        if(sysUser == null){
+            return null;
+        }
+        if (sysUser.getPassword().equals(password)){
+
+            return sysUser;
+        }
+        return null;
+    }
+
+    @Override
+    public int registerSysUser(String account,String password) {
+        //查询账号密码是否为空，为空返回0
+        if (account.equals("")||password.equals("")){
+            return 0;
+        }
+        //查询账号是否存在，存在则返回-1
+        if(sysUserDao.findByAccount(account) != null){
+            return -1;
+        }
+        //否则,插入新用户，返回1
+        sysUserDao.insertNewSysUser(account,password);
+        return 1;
     }
 }
