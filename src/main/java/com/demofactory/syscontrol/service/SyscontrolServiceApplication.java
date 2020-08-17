bb910345d55540350fee385071b5be40b1de7bc8
WCHCCC <1923366766@qq.com>
2020-08-17 19:30:27 +0800

    dubbo+zk集成

--- src/main/java/com/demofactory/syscontrol/service/SyscontrolServiceApplication.java
+++ src/main/java/com/demofactory/syscontrol/service/SyscontrolServiceApplication.java
@@ -1,9 +1,11 @@
 package com.demofactory.syscontrol.service;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan({"com.demofactory.syscontrol.dao","com.baomidou.mybatisplus.samples.quickstart.mapper"})
 public class SyscontrolServiceApplication {
 
     public static void main(String[] args) {
